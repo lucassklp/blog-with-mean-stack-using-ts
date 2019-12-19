5663edd1021ff514bdfb9523714e175d2eeca921
Lucas Simas <simas.lucas@hotmail.com>
2019-12-19 16:56:51 +0000

    Initializing project with TS and hot-heload

---
+++ src/app.ts
@@ -0,0 +1,17 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send({
+      _id: "123",
+  });
+});
+
+app.listen(port, err => {
+  if (err) {
+    return console.error(err);
+  }
+  return console.log(`Server is listening on ${port}`);
+});
