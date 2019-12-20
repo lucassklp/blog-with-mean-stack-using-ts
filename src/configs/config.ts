b9f08e8f1043533a5d24411efac065b7ba11cf78
Lucas Simas <simas.lucas@hotmail.com>
2019-12-20 15:51:39 +0000

    Update config to NodeJS defaults

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -1,6 +1,6 @@
 import fs from "fs"
 
-let environment = process.env.NODE_JS_ENV || "dev";
+let environment = process.env.NODE_ENV || "development";
 let fileName = `${environment}${environment == "" ? "" : "-"}config.json`;
 
 export const config = JSON.parse(fs.readFileSync(`${__dirname}/${fileName}`, 'utf-8'));
